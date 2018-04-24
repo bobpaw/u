76f433e1509f633f97a99dad10f2376e103548db
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-04-24 09:12:34 -0500

    Updated gitpullall.sh and added scripts
    Added depth option to gitpullall.sh
    Added everything.sh
    Changed .bashrc in configs.tar.gz
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

---
+++ everything.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Halsey
+
+/bin/echo -e '\e[0;34mEverything. His pills, his hands, his jeans.\e[0m\n\e[1;30mEverything. His hair, his smoke, his dreams.\e[0m'
--- gitpullall.sh
+++ gitpullall.sh
@@ -4,11 +4,29 @@
 ## Update all git repositories in current directory
 ##
 
-for i in $(find -maxdepth 1 -type d | sed 's/\.\/\(.*\)/\1/'| grep -v "^.*\..*" | tr '\n' ' '); do {
-  if [ -d ./${i}/.git ]; then
-    echo "Pulling repository in folder ${i}"
-    cd ${i}
-    git pull
-    cd ..
+for i in $*; do
+    case $i in
+        -d*)
+            max=$(echo ${i} | cut -d'd' -f'2')
+            ;;
+        *)
+            dirs=${dirs} ${i}
+            ;;
+    esac
+done
+
+max=${max:-1}
+dirs=${dirs:-.}
+
+for topdir in $dirs; do
+  if cd $topdir; then
+    for i in $(find -maxdepth ${max} -type d | sed 's/\.\/\(.*\)/\1/'| grep -v "^.*\..*" | tr '\n' ' '); do
+      if [ -d ./${i}/.git ]; then
+        echo "Pulling repository in folder ${i}"
+        cd ${i}
+        git pull
+        cd ..
+      fi
+    done
   fi
-} done
+done
