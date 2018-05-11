e9fc50b85f32073867b25ea2a8a4571e79f9de8a
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-05-11 14:40:01 -0500

    Tried autoupdating
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- gitpullall.sh
+++ gitpullall.sh
@@ -4,26 +4,13 @@
 ## Update all git repositories in current directory, or specified directories
 ##
 
-tempfile=$(mktemp "${TMPDIR:-/tmp/}tmp.$(basename $0).XXXXXXX")
-
-curl -L -s -o ${tempfile} https://github.com/bobpaw/u/raw/master/gitpullall.sh
-
-if [ "$(diff gitpullall.sh ${tempfile} --unchanged-line-format='' --old-line-format='')" ]; then
-  echo -n "There is a newer version available. Use it? [y/n]: "
-  read answer
-  if [ "${answer}" = "y" ]; then
-    cp ${tempfile} "$(dirname $0)/gitpullall-new.sh"
-    echo "mv gitpullall-new.sh gitpullall.sh" >> gitpullall-fix.sh
-    echo "$(readlink -f /proc/$$/exe) gitpullall.sh" >> gitpullall-fix.sh
-    echo "rm -- $0" >> gipullall-fix.sh
-    exec $(readlink -f /proc/$$/exe) gitpullall-new.sh
-  fi
-fi
-
 for i in $*; do
     case $i in
         -d*)
             max=$(echo ${i} | cut -d'd' -f'2')
+            ;;
+        -n)
+            dont=true
             ;;
         *)
             dirs=${dirs} ${i}
@@ -33,16 +20,33 @@ done
 
 max=${max:-1}
 dirs=${dirs:-.}
+dont=${dont:-false}
+
+if [ "${dont}" = "false" ]; then
+    tempfile=$(mktemp "${TMPDIR:-/tmp/}tmp.$(basename $0).XXXXXXX")
+
+    curl -L -s -o ${tempfile} https://github.com/bobpaw/u/raw/master/gitpullall.sh
+
+    diff gitpullall.sh ${tempfile} --unchanged-line-format='' --old-line-format=''
+    if [ "$(diff gitpullall.sh ${tempfile} --unchanged-line-format='' --old-line-format='')" ]; then
+        echo -n "There is a newer version available. Use it? [y/n]: "
+        read answer
+        if [ "${answer}" = "y" ]; then
+            exec $(readlink -f /proc/$$/exe) gitpullall-new.sh -n
+            exit 1
+        fi
+    fi
+fi
 
 for topdir in $dirs; do
-  if cd $topdir; then
-    for i in $(find -maxdepth ${max} -type d | sed 's/\.\/\(.*\)/\1/'| grep -v "^.*\..*" | tr '\n' ' '); do
-      if [ -d ./${i}/.git ]; then
-        echo "Pulling repository in folder ${i}"
-        cd ${i}
-        git pull
-        cd ..
-      fi
-    done
-  fi
+    if cd $topdir; then
+        for i in $(find -maxdepth ${max} -type d | sed 's/\.\/\(.*\)/\1/'| grep -v "^.*\..*" | tr '\n' ' '); do
+            if [ -d ./${i}/.git ]; then
+                echo "Pulling repository in folder ${i}"
+                cd ${i}
+                git pull
+                cd ..
+            fi
+        done
+    fi
 done
