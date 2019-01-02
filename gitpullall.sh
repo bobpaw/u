bbbe3d2ad8d41b2e9d9da7be01a6c5be0ccfa9df
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-01-01 22:24:28 -0600

    Use GitHub APIv3 to check if the other file is newer
    Only check for updates when explicitly specified
    Also some fun hybrid Python scripting to compare dates and examine the JSON
    Try to use temp files to not use the weird bash <() syntax
    Use pushd and popd over cd
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- gitpullall.sh
+++ gitpullall.sh
@@ -9,8 +9,8 @@ for i in $*; do
         -d*)
             max="$(echo ${i} | cut -d'd' -f'2')"
             ;;
-        -n)
-            dont="true"
+        -u)
+            check="true"
             ;;
         *)
             dirs="${dirs} ${i}"
@@ -20,33 +20,66 @@ done
 
 max=${max:-1}
 dirs=${dirs:-.}
-dont=${dont:-false}
+check=${check:-false}
 
-if [ "${dont}" = "false" ]; then
-    curl -Ls -o "$0-new" https://github.com/bobpaw/u/raw/master/gitpullall.sh
-    diff <(sed 's/\\r\\n/\\n/' "$0") <(sed 's/\\r\\n/\\n/' "$0-new") --unchanged-line-format='' --old-line-format=''
-    if [ "$(diff <(sed 's/\\r\\n/\\n/' $0) <(sed 's/\\r\\n/\\n/' $0-new) --unchanged-line-format='' --old-line-format='')" ]; then
-        echo -n "There is a newer version available. Use it? [y/n]: "
+if [ "${check}" = "true" ]; then
+    self_history="$(curl -s https://api.github.com/repos/bobpaw/u/commits?path=gitpullall.sh)"
+    self_reallast="$(stat -c %Y $0)"
+    git_last=$(python3 -c "
+import json
+import dateutil.parser as dp
+hist_string = '''$(echo ${self_history} | sed 's/\\/\\\\/g')'''
+date = json.loads(hist_string)[0]['commit']['author']['date']
+parsed = dp.parse(date)
+print(parsed.strftime('%s'))
+"
+)
+    if [ "${git_last}" -gt "${self_reallast}" ]; then
+        echo -n "There is a newer version available from GitHub. Try to get it? [y/n]: "
         read answer
         if [ "${answer}" = "y" ]; then
-            mv "$0-new" "$0"
-            chmod +x "$0"
-            exec "$0" -n "$@"
-            exit 1
-        fi
-    fi
-    rm -f "$0-new"
-fi
+            curl -Ls -o "$0-new" https://github.com/bobpaw/u/raw/master/gitpullall.sh
+            unix_self=$(mktemp)
+            unix_new=$(mktemp)
+            sed 's/\\r\\n/\\n/' "$0" > ${unix_self}
+            sed 's/\\r\\n/\\n/' "$0-new" > ${unix_new}
+            echo "Diff:"
+            diff ${unix_self} ${unix_new}
+            if [ "$(diff ${unix_self} ${unix_new})" ]; then
+                echo -n "Use the newer version? [y/n]: "
+                read answer
+                if [ "${answer}" = "y" ]; then
+                    cat > "$0-mid" <<EOF
+mv "$0-new" "$0"
+chmod +x "$0"
+exec "$0" $(awk '{ for (i = 0; i < NF; ++i) if (i != "-u" ) printf "%s ", $i }')
+exit 1
+EOF
+                    exec "$0-mid"
+                fi
+            fi
+            rm -f "$0-new" "${unix_self}" "${unix_new}"
+        fi # use it?
+    fi # Older?
+fi # Check flag?
 
-for topdir in $dirs; do
-    if cd $topdir; then
+for topdir in ${dirs}; do
+    if pushd ${topdir} > /dev/null; then
         for i in $(find -maxdepth ${max} -type d | sed 's/\.\/\(.*\)/\1/'| grep -v "^.*\..*" | tr '\n' ' '); do
             if [ -d ./${i}/.git ]; then
-                echo "Pulling repository in folder ${i}"
-                cd ${i}
-                git pull --all
-                cd ..
+                echo -n "Pulling repository in folder ${i}"
+                pushd ${i} > /dev/null
+                git_output="$(git pull --all | tail -n+2)"
+                if [ "$(echo $git_output | awk 'END {print FNR}')" -eq 1 ]; then
+                    echo " - ""$git_output"
+                else
+                    echo
+                    echo "$git_output"
+                fi
+                popd > /dev/null
             fi
         done
     fi
+    popd > /dev/null
 done
+
