536c6291662ddd50bdb327258a926f1fc047df45
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-05-10 13:02:43 -0500

    Added color support to gitpullall.sh
    Install py-dateutil if not already
    Include curdir in search path
    If stdout is a tty, print color. Otherwise, print no color
    Use wc for line count
    Add some comments for final fi's and done's
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- gitpullall.sh
+++ gitpullall.sh
@@ -3,6 +3,11 @@
 ##
 ## Update all git repositories in current directory, or specified directories
 ##
+## Options:
+## -h Display usage
+## -u Try to auto-update self
+## -d Set max search depth
+##
 
 for i in $*; do
     case $i in
@@ -11,6 +16,10 @@ for i in $*; do
             ;;
         -u)
             check="true"
+            ;;
+        -h)
+            echo -e "Usage: " $0 " [-h | -u] [-d INT ]\n-h\tDisplay help\n-u\tCheck for updates\n-d INT\tMax depth"
+            exit 0
             ;;
         *)
             dirs="${dirs} ${i}"
@@ -25,6 +34,7 @@ check=${check:-false}
 if [ "${check}" = "true" ]; then
     self_history="$(curl -s https://api.github.com/repos/bobpaw/u/commits?path=gitpullall.sh)"
     self_reallast="$(stat -c %Y $0)"
+    python3 -m pip install --user py-dateutil > /dev/null
     git_last=$(python3 -c "
 import json
 import dateutil.parser as dp
@@ -67,21 +77,31 @@ fi # Check flag?
 
 for topdir in ${dirs}; do
     if pushd ${topdir} > /dev/null; then
-        for i in $(find -maxdepth ${max} -type d | sed 's/\.\/\(.*\)/\1/'| grep -v "^.*\..*" | tr '\n' ' '); do
+        for i in $(find -maxdepth ${max} -type d | sed 's/\.\/\(.*\)/\1/'| grep -v "^.*\..*" | tr '\n' ' ') .; do
             if [ -d ./${i}/.git ]; then
                 echo -n "Pulling repository in folder ${i}"
                 pushd ${i} > /dev/null
-                git_output="$(git pull --all |& tail -n+2)"
-                if [ "$(echo $git_output | awk 'END {print FNR}')" -eq 1 ]; then
-                    echo " - ""$git_output"
+                git_outputc="$(script -q /dev/null -c 'git -c color.ui=always pull --all' | cat)"
+                git_output="$(sed 's/\x1b\[[0-9;]*[mGK]//g; s/\r$//; /done\.$/d' <<EOF | tail -n+2
+${git_outputc}
+EOF
+)"
+                if [ "$(wc -l <<EOF
+${git_output}
+EOF
+)" -eq 1 ]; then
+                    echo " - ${git_output}"
+                elif [ -t 1 ]; then
+                    echo
+                    echo "${git_outputc}"
                 else
                     echo
-                    echo "$git_output"
+                    echo "${git_output}"
                 fi
                 popd > /dev/null
-            fi
-        done
-    fi
+            fi # git directory?
+        done # Every dir in $topdir
+    fi # $topdir exists?
     popd > /dev/null
-done
+done # Each topdir
 
