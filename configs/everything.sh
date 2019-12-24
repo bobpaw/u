53a126cfa077103c35f5fb75c4a080702b5f4689
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-12-24 17:07:14 -0600

    Don't check for login shell
    Assume checks have been performed correctly in other files
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- configs/everything.sh
+++ configs/everything.sh
@@ -2,6 +2,4 @@
 
 # Halsey
 
-if echo "$-" | grep -q 'i'; then
-  /bin/echo -e '\e[0;34mEverything. His pills, his hands, his jeans.\e[0m\n\e[1;30mEverything. His hair, his smoke, his dreams.\e[0m'
-fi
+/bin/echo -e '\e[0;34mEverything. His pills, his hands, his jeans.\e[0m\n\e[1;30mEverything. His hair, his smoke, his dreams.\e[0m'
--- configs/flag.sh
+++ configs/flag.sh
@@ -2,13 +2,11 @@
 
 # Bi pride flag (12 long), only print for login shell (ie ssh/tty).
 
-if echo "$-" | grep -q 'i'; then
-	/bin/echo -e "O"
-	/bin/echo -e "|\e[48;5;199m            \e[0m"
-	/bin/echo -e "|\e[48;5;199m            \e[0m"
-	/bin/echo -e "|\e[48;35;45m            \e[0m"
-	/bin/echo -e "|\e[48;5;20m            \e[0m"
-	/bin/echo -e "|\e[48;5;20m            \e[0m"
-	/bin/echo -e "|"
-	/bin/echo -e "|"
-fi
+/bin/echo -e "O"
+/bin/echo -e "|\e[48;5;199m            \e[0m"
+/bin/echo -e "|\e[48;5;199m            \e[0m"
+/bin/echo -e "|\e[48;35;45m            \e[0m"
+/bin/echo -e "|\e[48;5;20m            \e[0m"
+/bin/echo -e "|\e[48;5;20m            \e[0m"
+/bin/echo -e "|"
+/bin/echo -e "|"
