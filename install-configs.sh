5476ff761c05c3de2c602b61770b0a9707c9c1ea
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-12-25 12:24:35 -0600

    Made the install-configs script way better
    Ask about files that already exist
    Set it to not copy backups by default
    Be optionally verbose
    Added option to assume yes or no
    Added quiet
    Do all the fixing duplicate stuff at the end
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- install-configs.sh
+++ install-configs.sh
@@ -6,18 +6,34 @@
 ##
 ## Options:
 ##
+## -b Copy backups (default is no)
 ## -i PREFIX Set prefix to install files into (default: ~)
 ## --prefix=PREFIX
 ## -h Display help
+## -n Never replace destination files
+## -q Quiet mode (Implies -n unless -y is specified)
 ## -u Display usage
+## -v Verbose mode
+## -y Always replace destination files
 ##
 
+BACKUPS=0
 NEXT_PREFIX=0
 PREFIX=$HOME
 FOLDER="configs"
+REPLACE_ALL=0
+QUIET=0
+VERBOSE=0
 
 for opt in $*; do
 	case $opt in
+		-b)
+			BACKUPS=1
+			;;
+		-h)
+			echo "Full help not yet implemented"
+			exit 0
+			;;
 		-i)
 			NEXT_PREFIX=1
 			;;
@@ -28,13 +44,22 @@ for opt in $*; do
 				PREFIX="$(echo $opt | sed 's/^--prefix=//')"
 			fi
 			;;
+		-n)
+			REPLACE_ALL=-1
+			;;
+		-q)
+			QUIET=1
+			[ "$REPLACE_ALL" -eq 0 ] && REPLACE_ALL=-1
+			;;
 		-u)
-			echo "Usage: $0 [-hu] [-i PREFIX] [FOLDER]"
+			echo "Usage: $0 [-bhnquvy] [-i PREFIX] [FOLDER]"
 			exit 0
 			;;
-		-h)
-			echo "Full help not yet implemented"
-			exit 0
+		-v)
+			VERBOSE=1
+			;;
+		-y)
+			REPLACE_ALL=1
 			;;
 		*)
 			if [ "$NEXT_PREFIX" -eq 1 ]; then
@@ -44,9 +69,66 @@ for opt in $*; do
 	esac
 done
 
-file_list="$(find $FOLDER -type f ! -name README*)"
+if [ "$BACKUPS" -eq 0 ]; then
+	file_list="$(find $FOLDER -type f ! -name 'README*' ! -name '*~')"
+else
+	file_list="$(find $FOLDER -type f ! -name README*)"
+fi
+
+dup_list=""
+
 for i in $file_list; do
-	install -t $PREFIX $i && echo "Successfully installed $i" || { echo "Error installing $i"; exit 1; }
+	if [ -f "$PREFIX/$(basename $i)" ] && [ "$(diff $i $PREFIX/$(basename $i))" ]; then
+		dup_list="$dup_list $i"
+	elif [ -z "$(diff $i $PREFIX/$(basename $i))" ]; then
+		[ "$VERBOSE" -eq 1 ] && echo "Not replacing identical $(basename $i)"
+	else
+		install -t $PREFIX $i && { [ "$VERBOSE" -eq 1 ] && echo "Successfully installed $(basename $i)" || true; } || echo "Error installing $(basename $i)" > /dev/null >&2
+	fi
 done
 
+case_success=0
 
+for i in $dup_list; do
+	case_success=0
+	if [ "$REPLACE_ALL" -eq 0 ]; then
+		echo -n "File $(basename $i) already exists. $PREFIX/$(basename $i) is "
+		if [ "$(stat -c '%Y' $i)" -lt "$(stat -c '%Y' $PREFIX/$(basename $i))" ]; then
+			echo "newer."
+		else
+			echo "older."
+		fi
+		until [ "$case_success" -eq 1 ]; do
+			read -p "Replace it? [(y)es/(n)o/(d)iff/(a)ll/ne(v)er]: " response
+			case $response in
+				y)
+					install -t $PREFIX $i && { [ "$VERBOSE" -eq 1 ] && echo "Successfully installed $(basename $i)" || true; } || echo "Error installing $(basename $i)" > /dev/null >&2
+					case_success=1
+					;;
+				n)
+					[ "$VERBOSE" -eq 1 ] && echo "Not installing $(basename $i)"
+					case_success=1
+					;;
+				a)
+					REPLACE_ALL=1
+					install -t $PREFIX $i && { [ "$VERBOSE" -eq 1 ] && echo "Successfully installed $(basename $i)" || true; } || echo "Error installing $(basename $i)" > /dev/null >&2
+					case_success=1
+					;;
+				v)
+					REPLACE_ALL=-1
+					[ "$VERBOSE" -eq 1 ] && echo "Not installing $(basename $i)"
+					case_success=1
+					;;
+				d)
+					echo "Not yet implemented"
+					;;
+				*)
+					;;
+			esac
+		done
+	elif [ "$REPLACE_ALL" -eq -1 ]; then
+		[ "$VERBOSE" -eq 1 ] && echo "Not installing $(basename $i)"
+	else
+		install -t $PREFIX $i && { [ "$VERBOSE" -eq 1 ] && echo "Successfully installed $(basename $i)" || true; } || echo "Error installing $(basename $i)" > /dev/null >&2
+	fi
+done
