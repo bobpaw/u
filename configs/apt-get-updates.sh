f41962f7ce477120a8dc0ad2461f7f2c4b039d5f
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-01-01 20:51:32 -0600

    Always use cache in HOME
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- configs/apt-get-updates.sh
+++ configs/apt-get-updates.sh
@@ -11,13 +11,13 @@ else
 	apt_date="$cache_date"
 fi
 
-if ! { [ -f .apt-get-updates.sh-cache ] && [ "$(stat -c '%Y' .apt-get-updates.sh-cache)" -gt "$apt_date" ]; } then
+if ! { [ -f ~/.apt-get-updates.sh-cache ] && [ "$(stat -c '%Y' .apt-get-updates.sh-cache)" -gt "$apt_date" ]; } then
 	# Check if there are updates and tell me if there are
 	APT_GET_THINGS="$(apt-get upgrade -s | awk 'okaynow == 1 {print} /[0-9]+ upgraded/ {okaynow=1}')"
 	if [ "${APT_GET_THINGS}" ]; then
-		echo "${APT_GET_THINGS}" | awk 'seen[$2]++ {} END {for (item in seen) print "apt-get: " item}' > .apt-get-updates.sh-cache
+		echo "${APT_GET_THINGS}" | awk 'seen[$2]++ {} END {for (item in seen) print "apt-get: " item}' > ~/.apt-get-updates.sh-cache
 	else
-		touch .apt-get-updates.sh-cache
+		touch ~/.apt-get-updates.sh-cache
 	fi
 fi
-cat .apt-get-updates.sh-cache
+cat ~/.apt-get-updates.sh-cache
