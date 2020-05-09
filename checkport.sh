c5190d43e2108de6b6d2e95f74a2220ac5f27158
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-05-09 18:14:44 -0500

    Changed checkport.sh to tabs
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- checkport.sh
+++ checkport.sh
@@ -15,62 +15,62 @@ PORT_OPEN=
 ERROR=
 
 for i in $*; do
-    case ${i} in
-        -q)
-            QUIET=true
-            ;;
-        -p*)
-            PORTS=$(echo ${i} | sed 's/^-p\([0-9,-]\+\)$/\1/')
-            ;;
-        *)
-            HOST=${i}
-    esac
+	case ${i} in
+		-q)
+			QUIET=true
+			;;
+		-p*)
+			PORTS=$(echo ${i} | sed 's/^-p\([0-9,-]\+\)$/\1/')
+			;;
+		*)
+			HOST=${i}
+	esac
 done
 
 if [ "${HOST}" ]; then
-    text="`nmap -oG - -p${PORTS} ${HOST} | cat`"
-    if echo ${text} | grep -q 'Status: Up'; then
-        STATUS="Up"
-    else
-        STATUS="Down"
-    fi
-    if [ -z "${QUIET}" ]; then
-        echo "Host Status: ${STATUS}"
-    fi
-    if [ "${PORTS}" -a "${STATUS}" = "Up" ]; then
-        for PORT in $(echo ${PORTS}|tr ',' ' '); do
-            if grep "${PORT}/open" <(echo $text) > /dev/null; then
-                if [ -z "${QUIET}" ]; then
-                    echo "Port ${PORT} is open."
-                fi
-                if [ -z "${PORT_OPEN}" ]; then
-                    PORT_OPEN=true
-                fi
-            else
-                if [ -z "${QUIET}" ]; then
-                    echo "Port ${PORT} is closed."
-                fi
-                PORT_OPEN=false
-            fi
-        done
-    else
-        if [ -z "${QUIET}" ]; then
-            echo "No port provided."
-        fi
-        ERROR=true
-    fi
+	text="`nmap -oG - -p${PORTS} ${HOST} | cat`"
+	if echo ${text} | grep -q 'Status: Up'; then
+		STATUS="Up"
+	else
+		STATUS="Down"
+	fi
+	if [ -z "${QUIET}" ]; then
+		echo "Host Status: ${STATUS}"
+	fi
+	if [ "${PORTS}" -a "${STATUS}" = "Up" ]; then
+		for PORT in $(echo ${PORTS}|tr ',' ' '); do
+			if grep "${PORT}/open" <(echo $text) > /dev/null; then
+				if [ -z "${QUIET}" ]; then
+					echo "Port ${PORT} is open."
+				fi
+				if [ -z "${PORT_OPEN}" ]; then
+					PORT_OPEN=true
+				fi
+			else
+				if [ -z "${QUIET}" ]; then
+					echo "Port ${PORT} is closed."
+				fi
+				PORT_OPEN=false
+			fi
+		done
+	else
+		if [ -z "${QUIET}" ]; then
+			echo "No port provided."
+		fi
+		ERROR=true
+	fi
 else
-    if [ -z "${QUIET}" ]; then
-        echo "No host provided."
-    fi
-    ERROR=true
+	if [ -z "${QUIET}" ]; then
+		echo "No host provided."
+	fi
+	ERROR=true
 fi
 if [ ! "${ERROR}" -a "${STATUS}" = "Up" -a "${PORT_OPEN}" ]; then
-    exit 0
+	exit 0
 elif [ ! "${ERROR}" -a "${STATUS}" = "Down" ]; then
-    exit 1
+	exit 1
 elif [ ! "${ERROR}" -a "${STATUS}" = "Up" -a ! "${PORT_OPEN}" ]; then
-    exit 2
+	exit 2
 else
-    exit 3
+	exit 3
 fi
