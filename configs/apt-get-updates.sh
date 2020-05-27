32b2225cfd72d6896f77771b02c218a0c71a7f98
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-05-26 22:49:06 -0500

    Fixed cache file updating
    Made if statement clearer in its condition
    Now if the new upgrade is empty, it empties the cache file
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- configs/apt-get-updates.sh
+++ configs/apt-get-updates.sh
@@ -5,13 +5,13 @@ which mawk >/dev/null && alias awk=mawk
 
 apt_date="$( { find /var/lib/apt/lists -type f -exec stat -c '%Y' '{}' \; 2>/dev/null; stat -c '%Y' /var/cache/apt/pkgcache.bin /var/log/apt/history.log; } | sort -n | tail -n1)"
 
-if ! { [ -f ~/.apt-get-updates.sh-cache ] && [ "$(stat -c '%Y' ~/.apt-get-updates.sh-cache)" -gt "$apt_date" ]; } then
+if ! [ -f ~/.apt-get-updates.sh-cache ] || [ "$(stat -c '%Y' ~/.apt-get-updates.sh-cache)" -lt "$apt_date" ]; then
 	# Check if there are updates and tell me if there are
 	APT_GET_THINGS="$(apt-get upgrade -s | awk 'okaynow == 1 {print} /[0-9]+ upgraded/ {okaynow=1}')"
 	if [ "${APT_GET_THINGS}" ]; then
 		echo "${APT_GET_THINGS}" | awk 'seen[$2]++ {} END {for (item in seen) print "apt-get: " item}' > ~/.apt-get-updates.sh-cache
 	else
-		touch ~/.apt-get-updates.sh-cache
+		: > ~/.apt-get-updates.sh-cache
 	fi
 fi
 cat ~/.apt-get-updates.sh-cache
