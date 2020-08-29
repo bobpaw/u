d6867c4b3e3b14820e32d3b8468c1a76a451d7e1
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-08-29 01:15:18 -0500

    Use getopts
    use tabs
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- connectping.sh
+++ connectping.sh
@@ -12,61 +12,68 @@ USER=
 QUIET=
 IFACE=
 
-for i in $*; do
-    case ${i} in
-        -p*|--port=*)
-            PORT=$(echo ${i} | sed 's/\(-p\|--port=\)\([[:digit:]]\+\)/\2/')
-            ;;
-        -q|--quiet)
-            QUIET=true
-            ;;
-        -u*|--user=*)
-            USER=$(echo ${i} | sed 's/\(-u\|--user=\)\([[:print:]]\+\)/\2/')
-            ;;
-        -i*|--interface=*)
-            IFACE=$(echo ${i} | sed 's/\(-i\|--interface=\)\(.\+\)/\2/')
-            ;;
-    esac
+while getopts 'i:p:qu:' opt; do
+	case $opt in
+		-p*|--port=*)
+			PORT=$(echo ${i} | sed 's/\(-p\|--port=\)\([[:digit:]]\+\)/\2/')
+			;;
+		-q|--quiet)
+			QUIET=true
+			;;
+		-u*|--user=*)
+			USER=$(echo ${i} | sed 's/\(-u\|--user=\)\([[:print:]]\+\)/\2/')
+			;;
+		-i*|--interface=*)
+			IFACE=$(echo ${i} | sed 's/\(-i\|--interface=\)\(.\+\)/\2/')
+			;;
+	esac
+done
+shift $((OPTIND - 1))
+
+while [ $# -gt 0 ]; do
+	case $1 in
+	--port?
+	shift
 done
 
 if [ "${IFACE}" ]; then
-    if [ -z "${QUIET}" ]; then
-        echo "Using interface: ${IFACE}"
-    fi
-    HOST="`tcpdump -i${IFACE} -c1 'icmp[icmptype] == icmp-echo or (icmp6 && ip6[40] == 128)' 2> /dev/null | cut -f3 -d' '`"
+	if [ -z "${QUIET}" ]; then
+		echo "Using interface: ${IFACE}"
+	fi
+	HOST="`tcpdump -i${IFACE} -c1 'icmp[icmptype] == icmp-echo or (icmp6 && ip6[40] == 128)' 2> /dev/null | cut -f3 -d' '`"
 else
-    HOST="`tcpdump -c1 'icmp[icmptype] == icmp-echo or (icmp6 && ip6[40] == 128)' 2> /dev/null | cut -f3 -d' '`"
+	HOST="`tcpdump -c1 'icmp[icmptype] == icmp-echo or (icmp6 && ip6[40] == 128)' 2> /dev/null | cut -f3 -d' '`"
 fi
 
 if [ $? -ne 0 ]; then
-    ERROR=true
+	ERROR=true
 fi
 
 if [ -z "${QUIET}" ]; then
-    echo "Host found: ${HOST}"
+	echo "Host found: ${HOST}"
 fi
 
 if ./checkport.sh -q -p${PORT} "${HOST}"; then
-    if [ ${PORT} -ne 22 ]; then
-      nc -c '/bin/sh' ${HOST} ${PORT}
-    else
-      if [ "${USER}" ]; then
-          if [ -z "${QUIET}" ]; then
-             echo "User Given: ${USER}"
-         fi
-         ssh ${USER}@${HOST}
-     else
-          ssh ${HOST}
-      fi
-    fi
+	if [ ${PORT} -ne 22 ]; then
+	  nc -c '/bin/sh' ${HOST} ${PORT}
+	else
+	  if [ "${USER}" ]; then
+		  if [ -z "${QUIET}" ]; then
+			 echo "User Given: ${USER}"
+		 fi
+		 ssh ${USER}@${HOST}
+	 else
+		  ssh ${HOST}
+	  fi
+	fi
 else
-    if [ -z "${QUIET}" ]; then
-        echo "Host is not listening on port 22"
-    fi
+	if [ -z "${QUIET}" ]; then
+		echo "Host is not listening on port 22"
+	fi
 fi
 
 if [ ERROR ]; then
-    exit 1
+	exit 1
 else
-    exit 0
+	exit 0
 fi
